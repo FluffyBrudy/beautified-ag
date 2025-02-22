e7b6a7d0390a993dd8ab6c5eac3b8ee6624acbbb
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-02-22 23:13:08 +0545

    add setup.py for setting command automatically

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-echo '#!/usr/bin/env python3' | cat - main.py > ~/.local/bin/bag
+python3 -m zipapp src/ -o bag -m "main:main" --python="$(which python3)"
+mv bag ~/.local/bin/bag
 chmod +x ~/.local/bin/bag
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 from arguments import *
 from initializer import init
