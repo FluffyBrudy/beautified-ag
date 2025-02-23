e12f66d763e24ced7b59609712b2e4a3874ddce5
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-02-23 19:26:35 +0545

    Prevent duplicate PATH entries in setup.sh
    - Updated setup.sh to check if ~/.local/bin is already in PATH before appending.
    - Prevents duplicate PATH modifications in ~/.bashrc.
    ng.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 python3 -m zipapp src/ -o bag -m "main:main" --python="$(which python3)"
 mv bag ~/.local/bin/bag
 chmod +x ~/.local/bin/bag
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+
+if ! grep -q 'export PATH="$HOME/.local/bin' ~/.bashrc; then
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+fi
+
 source ~/.bashrc
